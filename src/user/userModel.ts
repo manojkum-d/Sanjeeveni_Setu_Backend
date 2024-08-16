27f8315263be89d0c68005ff141f17634534dc8a
manoj-kd <91712321+manoj-kd@users.noreply.github.com>
2024-08-16 15:16:10 +0530

    start

--- src/user/userModel.ts
+++ src/user/userModel.ts
@@ -1,18 +1,17 @@
-// userModel.ts
-import mongoose from "mongoose";
+import mongoose, { Model, Schema } from "mongoose";
 import { User } from "./userTypes";
 
-const userSchema = new mongoose.Schema<User>({
-  username: { type: String, required: true, unique: true },
+const userSchema = new Schema<User>({
+  username: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   fullName: { type: String, required: true },
   dateOfBirth: { type: Date, required: true },
-  gender: { type: String, enum: ["Male", "Female", "Other"] },
-  phoneNumber: { type: String },
-  address: { type: String },
-  documents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Document" }],
-  qrCode: { type: mongoose.Schema.Types.ObjectId, ref: "QRCode" },
+  gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
+  phoneNumber: String,
+  address: String,
+  documents: [{ type: mongoose.Types.ObjectId, ref: "Document" }],
+  qrCode: { type: mongoose.Types.ObjectId, ref: "QrCode" },
   otp: String,
   otpExpiration: Date,
   phoneOtp: String,
@@ -22,4 +21,6 @@ const userSchema = new mongoose.Schema<User>({
   isHealthFormCompleted: { type: Boolean, default: false },
 });
 
-export default mongoose.model<User>("User", userSchema);
+const userModel: Model<User> = mongoose.model<User>("User", userSchema);
+
+export default userModel;
